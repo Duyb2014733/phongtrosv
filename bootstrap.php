3930ade2e71f113d63922f78241273257ecb112f
Duyb2014733 <ldduy038@gmail.com>
2023-11-17 17:23:33 +0700

    cập nhật

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/libraries/Psr4AutoloaderClass.php';
 $loader = new Psr4AutoloaderClass;
 $loader->register();
 // Các lớp có không gian tên bắt đầu với website/src nằm ở src
-$loader->addNamespace('phongtrsv\src', __DIR__ . '/src');
+$loader->addNamespace('phongtrosv\src', __DIR__ . '/src');
 try {
     $PDO = (new \phongtrosv\src\PDOFactory())->create([
         'dbhost' => 'localhost',
